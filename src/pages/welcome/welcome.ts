8440257012cda825c145d728846054981b13c224
guygauravtyagi <gtyagig786@gmail.com>
2018-06-08 15:55:33 +0530

    CSS stuff

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { LoginPage } from '../login/login';
+import { TabsPage } from '../tabs/tabs';
 import { SignupPage } from '../signup/signup';
 
 @IonicPage()
@@ -14,12 +14,12 @@ export class WelcomePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  login(){
-    this.navCtrl.push(LoginPage);
-    }
-  
-    signup(){
+  login() {
+    this.navCtrl.push(TabsPage, {}, { animate: false });
+  }
+
+  signup() {
     this.navCtrl.push(SignupPage);
-    }
+  }
 
 }
